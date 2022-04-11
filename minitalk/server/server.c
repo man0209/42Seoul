fdf3f52901377cf5174f03f66db192c7cb661127
Kounho Kim <kokim@c6r10s1.42seoul.kr>
2022-04-11 15:50:44 +0900

    ing

--- minitalk/server/server.c
+++ minitalk/server/server.c
@@ -6,50 +6,11 @@
 /*   By: kokim <kokim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 13:08:48 by kokim             #+#    #+#             */
-/*   Updated: 2022/04/06 16:43:18 by kokim            ###   ########.fr       */
+/*   Updated: 2022/04/11 13:42:33 by kokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *s)
-{
-	size_t	i;
-
-	i = 0;
-	if (s == NULL)
-	{
-		write(1, "(null)", 6);
-		return ;
-	}
-	while (s[i] != '\0')
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-}
-
-void	ft_putnbr(int nbr)
-{
-	if (nbr == -2147483648)
-	{
-		ft_putstr("-2147483648");
-		return ;
-	}
-	if (nbr < 0)
-	{
-		nbr *= -1;
-		ft_putchar('-');
-	}
-	if (nbr > 9)
-		ft_putnbr(nbr / 10);
-	ft_putchar((nbr % 10) + '0');
-}
 
 void	bit_to_str(int sig)
 {
@@ -73,9 +34,7 @@ int	main()
 
 	pid = getpid();
 
-	ft_putstr("pid : ");
-	ft_putnbr(pid);
-	ft_putchar('\n');
+	ft_printf("pid : %d\n", pid);
 	signal(SIGUSR1, bit_to_str);
 	signal(SIGUSR2, bit_to_str);
 	while (1)
--- minitalk/server/server.h
+++ minitalk/server/server.h
@@ -6,7 +6,7 @@
 /*   By: kokim <kokim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:02:32 by kokim             #+#    #+#             */
-/*   Updated: 2022/04/06 14:17:51 by kokim            ###   ########.fr       */
+/*   Updated: 2022/04/11 11:24:45 by kokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <unistd.h>
 # include <signal.h>
 # include <stdlib.h>
+# include "../library/libft/libft.h"
+# include "../library/printf/ft_printf.h"
 
 typedef struct s_request
 {
